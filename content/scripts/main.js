fe0ea61bd5013189230447bce978b3ba39b99db1
Omar Baiomay <baiomayomar@gmail.com>
2021-03-28 00:04:51 +0200

    add slider

---
+++ content/scripts/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        autoplayspeed: 1500,
+        smartspeed: 1500,
+        rtl:true
+    })
+});
